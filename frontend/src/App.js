daf2e5286cd9acd47da00083fd9d60948d955552
Leonardo8221 <r.leonardo0802@gmail.com>
2024-03-28 20:28:54 -0400

    Updat UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <div>
             <main className="relative mx-auto max-w-3xl px-4">
-                <h1 className="my-8 text-center text-4xl">
+                <h1 className="mt-[10rem] text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl ">
                     Agriculture AI Agent
                 </h1>
             </main>
