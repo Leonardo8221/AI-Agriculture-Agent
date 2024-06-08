c3863de91c529e513a644cea89f2e198ed8d7097
Leonardo8221 <r.leonardo0802@gmail.com>
2024-06-08 21:04:46 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-expressions */
+import React, { useState, useEffect } from "react";
 import Result from "./components/Result";
 import Home from "./components/Home";
 
-import './App.css'
+import "./App.css";
 
-function App() {
+function App(props) {
   const [showResult, setShowResult] = useState(false);
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    () => {
+      console.log("hello");
+      setCounter(1);
+    };
+  }, [props.visible]);
 
   return (
     <div className="flex max-w-7xl justify-center mx-auto">
